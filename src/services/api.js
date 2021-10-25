daa3273700ed5d208e9abc3fa889b0b4112db4f9
Luan Vitor <luan_vittor@hotmail.com>
2021-10-25 15:45:03 -0300

    01 Feito!

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,11 @@
 export async function getCategories() {
-  // Implemente aqui
+  const FETCH_CATEGORIES = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const REQUEST_JSON = await FETCH_CATEGORIES.json();
+  return REQUEST_JSON;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const FETCH_PRODUCTS = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}=${query}`);
+  const PRODUCT_JSON = await FETCH_PRODUCTS.json();
+  return PRODUCT_JSON;
 }
