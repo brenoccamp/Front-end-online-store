9fd06261229be7fea22b63ba98732c77f3ea2ad2
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-10-25 15:48:40 -0400

    03 Feito!

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
+import Cart from './pages/Cart';
 
-function App() {
-  return (
-    <div className="App">
-      <Home />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={ Home } />
+            <Route exact path="/cart" component={ Cart } />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class home extends React.Component {
   render() {
@@ -10,6 +11,13 @@ class home extends React.Component {
               type="text"
             />
           </div>
+          <Link to="/cart" data-testid="shopping-cart-button">
+            <button
+              type="button"
+            >
+              Carrinho
+            </button>
+          </Link>
           <p data-testid="home-initial-message">
             Digite algum termo de pesquisa ou escolha uma categoria.
           </p>
