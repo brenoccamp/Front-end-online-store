00ed76afd617651c0f3ad83fb5a2ce644b53f65c
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-10-27 18:09:12 -0400

    Requisito 10 FEITO! Testes passando do 1 ao 10! AGORA FOI!

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,28 +1,99 @@
 import React from 'react';
-import ProductCard from '../components/ProductCard';
-
+import { Link } from 'react-router-dom';
 class Cart extends React.Component {
   constructor() {
     super();
+    this.increaseValue = this.increaseValue.bind(this);
+    this.decreaseValue = this.decreaseValue.bind(this);
+    this.removeProduct = this.removeProduct.bind(this);
+    this.removeProductFromDecrease = this.removeProductFromDecrease.bind(this);
     this.state = {
       cart: '',
     };
   }
-
   componentDidMount() {
     this.loadCart();
   }
 
+  // https://pt.stackoverflow.com/questions/397104/atualizar-campo-de-uma-sele%C3%A7%C3%A3o-de-um-array-de-objetos-com-filter
+  increaseValue({ target }) {
+    const CART = JSON.parse(localStorage.getItem('Cart'));
+    const ID = target.id;
+    CART.reduce((acc, o) => acc.concat(ID.includes(o[0].id) ? Object.assign(o[1], { QUANTIDADE: o[1].QUANTIDADE + 1 }) : o), []);
+    localStorage.setItem('Cart', JSON.stringify(CART));
+    this.setState({ cart: CART });
+  }
   loadCart() {
     const LOAD_CART = JSON.parse(localStorage.getItem('Cart'));
     this.setState({ cart: LOAD_CART });
   }
-
+  decreaseValue({ target }) {
+    const CART = JSON.parse(localStorage.getItem('Cart'));
+    const ID = target.id;
+    const ITEM = CART.find((element) => element[0].id === target.id);
+    if (ITEM[1].QUANTIDADE === 1) {
+      this.removeProductFromDecrease(ID);
+    } else {
+      CART.reduce((acc, o) => acc.concat(ID.includes(o[0].id) ? Object.assign(o[1], { QUANTIDADE: o[1].QUANTIDADE - 1 }) : o), []);
+      localStorage.setItem('Cart', JSON.stringify(CART));
+      this.setState({ cart: CART });
+    }
+  }
+  removeProductFromDecrease(id) {
+    const { cart } = this.state;
+    const NEW_CART = cart.filter((element) => element[0].id !== id);
+    localStorage.setItem('Cart', JSON.stringify(NEW_CART));
+    this.setState({ cart: NEW_CART });
+    this.loadCart();
+  }
+  removeProduct({ target }) {
+    const { cart } = this.state;
+    const NEW_CART = cart.filter((element) => element[0].id !== target.id);
+    localStorage.setItem('Cart', JSON.stringify(NEW_CART));
+    this.loadCart();
+  }
   render() {
     const { cart } = this.state;
     if (cart.length > 0) {
       return cart.map((element) => (
-        <ProductCard key={ element.id } element={ element } />
+        <div key={ element[0].id }>
+          <Link
+            to={ { pathname: `/product-details/${element[0].id}`, state: { element } } }
+            data-testid="product-detail-link"
+          >
+            <div className="card" data-testid="product">
+              <h1 data-testid="shopping-cart-product-name">{ element[0].title }</h1>
+              <img src={ element[0].thumbnail } alt={ element[0].title } />
+              <h3>{`R$: ${element[0].price}`}</h3>
+            </div>
+          </Link>
+          <button
+            id={ element[0].id }
+            type="button"
+            data-testid="product-decrease-quantity"
+            onClick={ this.decreaseValue }
+          >
+            -
+          </button>
+          <p>Quantidade: </p>
+          <p data-testid="shopping-cart-product-quantity">{ element[1].QUANTIDADE }</p>
+          <button
+            id={ element[0].id }
+            type="button"
+            data-testid="product-increase-quantity"
+            onClick={ this.increaseValue }
+          >
+            +
+          </button>
+          <button
+            id={ element[0].id }
+            type="button"
+            data-testid=""
+            onClick={ this.removeProduct }
+          >
+            Remover Produto
+          </button>
+        </div>
       ));
     }
     return (
