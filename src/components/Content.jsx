15deff2b6677123085b6545de23693e031bf41ed
Luan Vitor <luan_vittor@hotmail.com>
2021-10-27 21:38:12 -0300

    13 na metade

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -4,11 +4,11 @@ import ProductCard from './ProductCard';
 
 export default class Content extends Component {
   render() {
-    const { result } = this.props;
+    const { result, teste } = this.props;
     return (
       <div>
         {result.map((element) => (
-          <ProductCard key={ element.id } element={ element } />
+          <ProductCard key={ element.id } element={ element } teste={ teste } />
         ))}
       </div>
     );
